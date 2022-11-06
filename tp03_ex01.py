2ec1a5a1064f82d023fb1131b8d2aeea69ba27ad
JeromeHumbert <54804629+JeromeHumbert@users.noreply.github.com>
2022-11-06 10:46:24 +0100

    Update tp03_ex01.py

--- tp03_ex01.py
+++ tp03_ex01.py
@@ -4,7 +4,7 @@ que 10 est un nombre porte-bonheur. Un nombre porte-bonheur est un nombre
 entier qui, lorsque l’on ajoute les carrés de chacun de ses chiffres,
 puis les carrés des chiffres de ce résultat et ainsi de suite
 jusqu’à l’obtention du résultat 1.
-Le calcul s’arrête lorsque le chiffre devient inférieur à 10.
+Le calcul s’arrête lorsque le chiffre devient <= 10.
 """
 
 #Fonctions
